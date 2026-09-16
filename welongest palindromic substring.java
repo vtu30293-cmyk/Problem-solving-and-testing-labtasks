56371e57b59e5b8e0ce50b387e73f56f38115ea0
vtu30293-cmyk <vtu30293@veltech.edu.in>
2026-09-16 11:15:05 +0530

    Create welongest palindromic substring.java

---
+++ welongest palindromic substring.java
@@ -0,0 +1,43 @@
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        String res = "";
+
+        for(int i = 0;i<n;i++){
+            // Odd length palindrome
+            int st = i,end = i;
+            while(st>=0&&end<n&&s.charAt(st)==s.charAt(end)){
+                st--;
+                end++;
+            }
+            String temp = s.substring(st+1,end);
+            if(temp.length()>res.length())res=temp;
+
+            // Even length palindrome
+            st = i;
+            end = i+1;
+            while(st>=0&&end<n&&s.charAt(st)==s.charAt(end)){
+                st--;
+                end++;
+            }
+            temp = s.substring(st+1,end);
+            if(temp.length()>res.length())res=temp;
+        }
+
+        return res;
+    }
+}
